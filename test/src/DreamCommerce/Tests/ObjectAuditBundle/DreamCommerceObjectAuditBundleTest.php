d6eb0f7f83c3016ff1d958f594ed5112e9773492
Michal Korus <michal.korus@shoper.pl>
2017-03-16 09:25:20 +0100

    fix commands

--- test/src/DreamCommerce/Tests/ObjectAuditBundle/DreamCommerceObjectAuditBundleTest.php
+++ test/src/DreamCommerce/Tests/ObjectAuditBundle/DreamCommerceObjectAuditBundleTest.php
@@ -31,13 +31,17 @@
 namespace DreamCommerce\Tests\ObjectAuditBundle;
 
 use DateTime;
+use Doctrine\Common\Persistence\ObjectManager;
 use DreamCommerce\Bundle\ObjectAuditBundle\DependencyInjection\DreamCommerceObjectAuditExtension;
 use DreamCommerce\Component\Common\Factory\DateTimeFactory;
 use DreamCommerce\Component\ObjectAudit\Manager\ObjectAuditManagerInterface;
+use DreamCommerce\Component\ObjectAudit\Manager\ResourceAuditManagerInterface;
 use DreamCommerce\Component\ObjectAudit\Manager\RevisionManagerInterface;
 use DreamCommerce\Component\ObjectAudit\Model\Revision;
 use DreamCommerce\Fixtures\ObjectAuditBundle\Entity\AuditEntity;
+use DreamCommerce\Fixtures\ObjectAuditBundle\Entity\AuditResource;
 use JMS\Serializer\SerializerInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -108,7 +112,7 @@ class DreamCommerceObjectAuditBundleTest extends WebTestCase
         $this->assertEquals($expected, $actual);
     }
 
-    public function testSimpleAudit()
+    public function testSimpleEntityAudit()
     {
         /** @var ContainerInterface $container */
         $container = self::createClient()->getContainer();
@@ -150,5 +154,54 @@ class DreamCommerceObjectAuditBundleTest extends WebTestCase
         $auditEntity = $objectAuditManager->find(AuditEntity::class, $entity->getId(), $revision);
         $this->assertInstanceOf(AuditEntity::class, $auditEntity);
         $this->assertEquals('test 1', $auditEntity->getTest());
+    }
+
+    public function testSimpleResourceAudit()
+    {
+        /** @var ContainerInterface $container */
+        $container = self::createClient()->getContainer();
+
+        $dateTime = new DateTime('2011-01-02 12:13:14');
+        $dateTime2 = new DateTime('2012-01-02 12:13:14');
+        $dateTime3 = new DateTime('2013-01-02 12:13:14');
+
+        $dateTimeFactory = $this->createMock(DateTimeFactory::class);
+        $dateTimeFactory->method('createNew')
+            ->will($this->onConsecutiveCalls($dateTime, $dateTime2, $dateTime3));
+
+        $container->set('dream_commerce.datetime_factory', $dateTimeFactory);
+
+        /** @var RevisionManagerInterface $revisionManager */
+        $revisionManager = $container->get(DreamCommerceObjectAuditExtension::ALIAS . '.revision_manager');
+        $revision = $revisionManager->getRevision();
+
+        /** @var FactoryInterface $resourceFactory */
+        $resourceFactory = $container->get('dream_commerce.factory.test_audit');
+        /** @var ObjectManager $resourceManager */
+        $resourceManager = $container->get('dream_commerce.manager.test_audit');
+
+        /** @var AuditResource $resource */
+        $resource = $resourceFactory->createNew();
+        $resource->setTest('test 1');
+        $resourceManager->persist($resource);
+        $resourceManager->flush();
+
+        $revision2 = $revisionManager->getRevision();
+        $this->assertNotEquals($revision, $revision2);
+        $this->assertEquals($revision->getCreatedAt(), $dateTime);
+
+        $resource->setTest('test 2');
+        $resourceManager->flush();
+
+        $revision3 = $revisionManager->getRevision();
+        $this->assertNotEquals($revision3, $revision2);
+        $this->assertEquals($revision2->getCreatedAt(), $dateTime2);
+
+        /** @var ResourceAuditManagerInterface $resourceAuditManager */
+        $resourceAuditManager = $container->get(DreamCommerceObjectAuditExtension::ALIAS . '.resource_manager');
+        /** @var AuditResource $auditResource */
+        $auditResource = $resourceAuditManager->find('dream_commerce.test_audit', $resource->getId(), $revision);
+        $this->assertInstanceOf(AuditResource::class, $auditResource);
+        $this->assertEquals('test 1', $auditResource->getTest());
     }
 }
