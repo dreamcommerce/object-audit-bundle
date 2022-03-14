1b91f5c3938303ce37fc2f7045e00fe4816c5872
Martynowski <wojciech.martynowski@dreamcommerce.com>
2022-03-14 08:38:47 +0100

    fix

--- src/DreamCommerce/Component/ObjectAudit/Doctrine/ORM/Subscriber/BuilderSubscriber.php
+++ src/DreamCommerce/Component/ObjectAudit/Doctrine/ORM/Subscriber/BuilderSubscriber.php
@@ -32,7 +32,7 @@ declare(strict_types=1);
 
 namespace DreamCommerce\Component\ObjectAudit\Doctrine\ORM\Subscriber;
 
-use Doctrine\EventSubscriber;
+use Doctrine\Common\EventSubscriber;
 use Doctrine\DBAL\Schema\Column;
 use Doctrine\DBAL\Schema\Table;
 use Doctrine\ORM\EntityManagerInterface;
